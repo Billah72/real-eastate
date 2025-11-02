8197ae86df2130473eb23da904fad01c8b93b7c2
Hedaitul6708 <tamimbabu16@gmail.com>
2025-11-02 23:50:35 +0600

    add Testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header.jsx'
 import About from './components/About.jsx'
+import Projects from './components/Projects.jsx'
+import Testimonials from './components/Testimonials.jsx'
 
 function App() {
   return (
     <div className='w-full overflow-hidden '>
       <Header />
       <About/>
+      <Projects/>
+      <Testimonials/>
     </div>
   )
 }
 
 export default App
+s
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden ' id='Testimonials'>
+      <h1 className='text-2xL sm:text-4xl font-bold mb-2 text-center'>
+        Customar <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonials</span>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Real Stories from those who found Home with us.</p>
+      </h1>
+    </div>
+  )
+}
+
+export default Testimonials
